83997f841ca9db78224f26ee9630bc84d0d16604
un5t0ppab13 <devbrights@gmail.com>
2015-10-21 13:15:56 -0700

    Hello World form React Connected: 3QYHWsSnlL0_DcsUAAAB

--- app-server.js
+++ app-server.js
@@ -7,5 +7,15 @@ app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
 // open port 3000 with express and notify user
-app.listen(3000);
+// app.listen(3000);
+var server = app.listen(3000);
+// socket server created, listening on port 3000
+var io = require('socket.io').listen(server);
+
+// when socket connects the event listener/handler with callback function taking in port 3000
+  // when new sockets connect log id
+io.sockets.on('connection', function (socket) {
+  console.log("Connected: %s", socket.id);
+}); 
+
 console.log("Polling server is running at 'http://localhost:3000'");
--- components/APP.js
+++ components/APP.js
@@ -1,8 +1,24 @@
+// require libraries
 var React = require('react');
+var io = require('socket.io-client');
+
+// APP.js main component, render is a must renders <APP />, 
+// componentWillMount adds a socket and a listener/event handler for connect event
+  // aka when connected, we use connect function here
 
 var APP = React.createClass({
-	render() {
-		return (<h1>Hello World form React</h1>);
+  
+  componentWillMount(){
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect', this.connect);
+  },
+  
+  connect() {
+    alert("Connected: " + this.socket.id);
+  },
+  
+  render() {
+    return (<h1> Hello World form React </h1>);
 	}
 });
 

1ec7b426d0b59eec1a279df44adf4b85d65c3626
un5t0ppab13 <devbrights@gmail.com>
2015-10-21 10:39:49 -0700

    npm init -> package.json, app-server.js, public/index.html, public/style.css

--- app-server.js
+++ app-server.js
@@ -1,9 +1,11 @@
+// require libaries, variable to call later
 var express = require('express');
-
 var app = express();
 
+// use/serve static files in folders listed
 app.use(express.static('./public'));
 app.use(express.static('./node_modules/bootstrap/dist'));
 
+// open port 3000 with express and notify user
 app.listen(3000);
 console.log("Polling server is running at 'http://localhost:3000'");
